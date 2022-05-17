de6b09968347d55f4dccd90d64457479b1c30566
Sajjaad <sajjaadsahodeea@gmail.com>
2022-05-17 18:35:27 +0400

    updated the views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,70 +1,42 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: %i[ show edit update destroy ]
+  before_action :set_list, only: [:edit, :show, :update]
 
-  # GET /lists or /lists.json
   def index
     @lists = List.all
   end
 
-  # GET /lists/1 or /lists/1.json
-  def show
-  end
+  def show; end
 
-  # GET /lists/new
   def new
     @list = List.new
   end
 
-  # GET /lists/1/edit
-  def edit
-  end
-
-  # POST /lists or /lists.json
   def create
     @list = List.new(list_params)
-
-    respond_to do |format|
-      if @list.save
-        format.html { redirect_to list_url(@list), notice: "List was successfully created." }
-        format.json { render :show, status: :created, location: @list }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @list.errors, status: :unprocessable_entity }
-      end
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /lists/1 or /lists/1.json
+  def edit; end
+
   def update
-    respond_to do |format|
-      if @list.update(list_params)
-        format.html { redirect_to list_url(@list), notice: "List was successfully updated." }
-        format.json { render :show, status: :ok, location: @list }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @list.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /lists/1 or /lists/1.json
-  def destroy
-    @list.destroy
-
-    respond_to do |format|
-      format.html { redirect_to lists_url, notice: "List was successfully destroyed." }
-      format.json { head :no_content }
+    if @list.update(list_params)
+      redirect_to list_path(@list)
+    else
+      render :new
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_list
-      @list = List.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def list_params
-      params.require(:list).permit(:name)
-    end
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :photo)
+  end
 end
