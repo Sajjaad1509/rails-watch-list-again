79da2a96e57188a32f444182a14da75efef65629
Sajjaad <sajjaadsahodeea@gmail.com>
2022-05-19 21:06:52 +0400

    Start with heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_05_19_150455) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookmarks", force: :cascade do |t|
     t.text "comment"
     t.integer "movie_id"
